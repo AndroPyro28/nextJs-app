fd90efdb32e3b0c6c7c0c1d42a19dd5842d05546
andropyro28 <menandroeugenio1028@gmail.com>
2022-07-01 02:40:22 -0400

    API_POST_REQUEST

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,12 +2,28 @@ import React, { useState } from "react";
 
 function Index() {
   const [comments, setComments] = useState([]);
+  const [commentInput, setCommentInput] = useState("");
+  
   const fetchComments = async () => {
     const data = await fetch("/api/comments").then((res) => res.json());
     setComments(data);
   };
+
+  const submitComment = async () => {
+    const data = await fetch('/api/comments/', {
+        method:'POST',
+        body: JSON.stringify(commentInput),
+        headers: {
+        'Content-Type':'application/json'
+        }
+    }).then(res => res.json());
+
+    console.log(data)
+  }
   return (
     <div>
+        <input type={"text"} value={commentInput} onChange={(e) => setCommentInput(e.target.value)} />
+        <button onClick={submitComment}>Submit Comment</button>
       <button onClick={fetchComments}>Load Comments</button>
 
       {comments.length > 0 &&
