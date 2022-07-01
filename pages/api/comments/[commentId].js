705e168b27c545528fbd8a770798e93f8fd9e0e3
andropyro28 <menandroeugenio1028@gmail.com>
2022-07-01 03:10:44 -0400

    API_DELETE_REQUEST

--- pages/api/comments/[commentId].js
+++ pages/api/comments/[commentId].js
@@ -1,9 +1,21 @@
 import comments from "../../../data/comments";
 function handler(req, res) {
-  const { commentId } = req.query;
-  const comment = comments.find((comment) => comment.id === Number(commentId));
 
-  return res.status(200).json( comment?.id ? comment : 'notFound' );
+    if(req.method === "GET") {
+        const comment = comments.find((comment) => comment.id === Number(commentId));
+        return res.status(200).json( comment?.id ? comment : 'notFound' );
+    }
+  const { commentId } = req.query;
+    
+  if(req.method === "DELETE") {
+    const deleteComment = comments.find((comment) => comment.id === Number(commentId));
+
+    const index = comments.find(comment => comment.id === Number(commentId))
+
+    comments.splice(index, 1)
+    
+    return res.status(200).json( deleteComment?.id ? deleteComment : 'notFound' );
+  }
 }
 
 export default handler;
