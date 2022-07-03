311fdba33c79fba9ab8499251214406f812e15dc
andropyro28 <menandroeugenio1028@gmail.com>
2022-07-03 03:49:04 -0400

    catchAllRoute

--- pages/api/comments/[commentId].js
+++ pages/api/comments/[commentId].js
@@ -1,11 +1,11 @@
 import comments from "../../../data/comments";
 function handler(req, res) {
+  const { commentId } = req.query;
 
     if(req.method === "GET") {
         const comment = comments.find((comment) => comment.id === Number(commentId));
         return res.status(200).json( comment?.id ? comment : 'notFound' );
     }
-  const { commentId } = req.query;
     
   if(req.method === "DELETE") {
     const deleteComment = comments.find((comment) => comment.id === Number(commentId));
